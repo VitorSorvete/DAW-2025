790523050022122e30138f97f2d63ab4a64368ac
Vitor dos Santos Moreira <vitorsorvete1910@gmail.com>
2025-11-06 21:56:02 -0300

    feat/aula 4 terminando de colocar os card e testando para ver se esta tudo ok

--- the-moviebox-react/src/cases/movies/components/movie-content.tsx
+++ the-moviebox-react/src/cases/movies/components/movie-content.tsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
-import { MovieService, type MovieDTO } from "../services/movie.service";
+import { useEffect, useState } from "react"
+import { MovieService, type MovieDTO } from "../services/movie.service"
+import { MovieCard } from "./movie-card";
 
 export function MovieContent() {
 
     const [movies, setMovies] = useState<MovieDTO[]>([]);
+    const [isloading, setIsloading] = useState<boolean>(false);
 
     useEffect(() => {
-        MovieService.list().then((result) => {
-            setMovies(result);
-        })
+        setIsloading(true);
+
+        MovieService.list().then((result) => {setMovies(result)})
+        .finally(() => {setIsloading(false)});
     }, []);
 
     return (
-        <section className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))]
-        gap-8 max-w-6xl mx-auto">
-            
-        <p>{JSON.stringify(movies)}</p>
-
+        isloading ? (<div className="bg-[#1c1c1c] flex justify-center items-center h-40">
+            <p className="text-2xl text-center text-white"> Carregando... </p>
+        </div>) : (
+        <section
+            className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] 
+            gap-8 max-w-6xl mx-auto">
+            {movies.map((movie, index)=>(<MovieCard key={index} movie={movie} />))}
         </section>
+        )
     )
 }
