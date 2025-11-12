4525ec01236d7c8a5ae2c4b3c0d654f01e62c8d4
Vitor dos Santos Moreira <vitorsorvete1910@gmail.com>
2025-11-12 16:36:19 -0300

    feat/alterando as imagem para que elas fiquem com o nome certo e a imagem de cada filme correto também

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -2,7 +2,7 @@ import { MovieContent } from "./cases/movies/components/movie-content"
 import { Footer } from "./components/layout/footer"
 import { Header } from "./components/layout/header"
 import { Navbar } from "./components/layout/navbar"
-import { Highlight } from "./components/layout/highlight"
+import { Highlight } from "./components/layout/Highlight"
 
 function App() {
 
--- the-moviebox-react/src/cases/movies/services/movie.service.ts
+++ the-moviebox-react/src/cases/movies/services/movie.service.ts
@@ -1,16 +1,62 @@
-import { api } from "../../../lib/axios";
-
 export type MovieDTO = {
     title: string;
     year: number;
     image: string;
 }
-
-const _ENDPOINT = '/movies?key=43c81b80'
+const movies: MovieDTO[] = [
+    {
+        title: "Lilo & Stitch",
+        year: 2025,
+        image: "./public/img/Y6pjszkKQUZ5uBbiGg7KWiCksJ.jpg",
+    },{
+        title: "Ballerina",
+        year: 2025,
+        image: "./public/img/2VUmvqsHb6cEtdfscEA6fqqVzLg.jpg",
+    },{
+        title: "M3GAN 2.0",
+        year: 2025,
+        image: "./public/img/4a63rQqIDTrYNdcnTXdPsQyxVLo.jpg",
+    },{
+        title: "Sinners",
+        year: 2025,
+        image: "./public/img/4CkZl1LK6a5rXBqJB2ZP77h3N5i.jpg",
+    },{
+        title: "Thunderbolts*",
+        year: 2025,
+        image: "./public/img/hqcexYHbiTBfDIdDWxrxPtVndBX.jpg",
+    },{
+        title: "F1",
+        year: 2025,
+        image: "./public/img/9PXZIUsSDh4alB80jheWX4fhZmy.jpg",  
+    },{
+        title: "The Ritual",
+        year: 2025,
+        image: "./public/img/ktqPs5QyuF8SpKnipvVHb3fwD8d.jpg",
+    },{
+        title: "Superman",
+        year: 2025,
+        image: "./public/img/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg",
+    },{   
+        title: "Heads of State",
+        year: 2025,
+        image: "./public/img/lVgE5oLzf7ABmzyASEVcjYyHI41.jpg",
+    },{
+        title: "The Hangman",
+        year: 2025, 
+        image: "./public/img/rij9Eh4RVXyPet8A1BNU8pDvtmu.jpg",
+    },{
+        title: "The Amateur",
+        year: 2025, 
+        image: "./public/img/SNEoUInCa5fAgwuEBMIMBGvkkh.jpg",
+    },{
+        title: "Jurassic World Rebirth",
+        year: 2025, 
+        image: "./public/img/q0fGCmjLu42MPlSO9OYWpI5w86I.jpg",
+    },
+];
 
 export const MovieService = {
     async list(): Promise<MovieDTO[]> {
-        const result = await api.get(_ENDPOINT);
-        return result.data;
+        return movies;
     }
 }
